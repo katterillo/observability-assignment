d660ecb914b5c7f7c49a0bb6927cacf69a234746
Kattrina <kattrina.erillo@gmail.com>
2023-04-24 22:42:10 -0400

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -1,21 +1,20 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 const { trace } = require("@opentelemetry/api");
-//Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-//Exporter
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+
 module.exports = (serviceName) => {
   const exporter = new JaegerExporter({
     serviceName: serviceName,
-    host: "localhost",
-    endpoint: 'http://localhost:14268/api/traces',
+    endpoint: "http://localhost:14268/api/traces",
   });
+
   const provider = new NodeTracerProvider({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
